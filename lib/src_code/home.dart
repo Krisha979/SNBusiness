809ce1d110e17a200fe06e1cf5b582041f512c21
ranabhawana <ranabhawana012@gmail.com>
2019-09-27 04:45:23 +0545

    final dashboard

--- lib/src_code/home.dart
+++ lib/src_code/home.dart
@@ -24,264 +24,192 @@ class HomeState extends State<Home> with SingleTickerProviderStateMixin {
     controller.forward();
   }
 
-  final carousel2 = Carousel(
-    boxFit: BoxFit.cover,
-    animationCurve: Curves.fastOutSlowIn,
+   final carousel2 = Carousel(
+     
+      boxFit: BoxFit.cover,
+      animationCurve: Curves.fastOutSlowIn,
     animationDuration: Duration(milliseconds: 2000),
-    dotSize: 4.0,
+    dotSize: 2.0,
     dotIncreasedColor: Color(0xFFFFFFFF),
     dotBgColor: Colors.black.withOpacity(0.5),
     autoplayDuration: Duration(seconds: 5),
     images: [
-      AssetImage('assets/image1.jpg'),
+      AssetImage('assets/image1.jpg',
+
+      ),
       AssetImage('assets/image2.jpg'),
       AssetImage('assets/image3.jpg'),
       AssetImage('assets/image4.jpg'),
     ],
     // dotBgColor: Colors.white.withOpacity(1),
+    
   );
-
-  @override
+   
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    //double screenHeight = MediaQuery.of(context).size.height;
+  //  double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: GestureDetector(
-          onTap: () {},
-          child: Container(
-            width: size.width,
-            height: size.height,
-            color: Color(0xFFd6d6d6),
+        body: SingleChildScrollView(
+        child:Container(
+           color: Color(0xFFd6d6d6),
+    //  height: size.height-62,
+      //width: size.width,
+     // color: Colors.grey,
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.fromLTRB(5, 0, 5, 0),
+             decoration: BoxDecoration(borderRadius: BorderRadius.circular(10.0), color: Colors.white),
+            //height: size.height/5 ,
+           // color: Colors.white,
             child: Column(
               children: <Widget>[
-                Wrap(
-                  direction: Axis.vertical,
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.only(top: 5.0),
-                      // margin: EdgeInsets.fromLTRB(0.0, .0, 00.0, 0.0),
-                      // height: size.height/6,
-                      //width: size.width/0.5,
-                      color: Colors.white,
-                      child: Column(
-                        children: <Widget>[
-                          new Image(
-                            image: new AssetImage("assets/logo.jpg"),
-                            height: size.width / 2.8,
-                            width: size.width,
-                          ),
-                          // Padding(
-                          //   padding: EdgeInsets.fromLTRB(10, 5, 10, 7)
-                          //),
-
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              "SN Business Solutions",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w400,
-                                  color: Colors.grey,
-                                  fontSize: 12.0),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+                new Image(
+                  image: new AssetImage("assets/logo.jpg"),
+                  height: size.height /4.8,
+                  width: size.width
                 ),
-                GestureDetector(
-                  onTap: () {},
-                  child: Wrap(children: <Widget>[
-                    Container(
-                      // height: size.height / 2.5,
-                      margin: EdgeInsets.all(1.0),
-                      padding: EdgeInsets.fromLTRB(5.0, 4.0, 5.0, 3.0),
-                      child: Material(
-                        color: Colors.white,
-                        elevation: 2.0,
-                        borderRadius: BorderRadius.circular(8.0),
-                        shadowColor: Color(0x802196f3),
-                        child: Column(
-                          children: <Widget>[
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: <Widget>[
-                                InkWell(
-                                  splashColor: Colors.red,
-                                  child: Column(children: <Widget>[
-                                    Wrap(children: <Widget>[
-                                      Padding(
-                                        padding: const EdgeInsets.all(25.0),
-                                        child: Image(
-                                          image: new AssetImage(
-                                              "assets/icon1.png"),
-                                          height: size.height / 14,
-                                          fit: BoxFit.contain,
-                                        ),
-                                      ),
-                                    ]),
-                                    Padding(
-                                      padding: EdgeInsets.only(top: 2.0),
-                                    ),
-                                    Text(
-                                      "Tasks",
-                                      style: TextStyle(
-                                          fontSize: 13, color: Colors.grey),
-                                    ),
-                                  ]),
-                                  onTap: () {},
-                                ),
-                                InkWell(
-                                  splashColor: Colors.red,
-                                  child: Column(
-                                    children: <Widget>[
-                                      Padding(
-                                        padding: const EdgeInsets.all(16.0),
-                                        child: Image(
-                                          image: new AssetImage(
-                                              "assets/invoice.png"),
-                                          height: size.height / 14,
-                                          fit: BoxFit.contain,
-                                        ),
-                                      ),
-                                      Padding(
-                                        padding: EdgeInsets.only(top: 15.0),
-                                      ),
-                                      Text(
-                                        "Invoices",
-                                        style: TextStyle(
-                                            fontSize: 13, color: Colors.grey),
-                                      )
-                                    ],
-                                  ),
-                                  onTap: () {},
-                                ),
-                                InkWell(
-                                  splashColor: Colors.red,
-                                  child: Column(
-                                    children: <Widget>[
-                                      Padding(
-                                        padding: const EdgeInsets.all(12.0),
-                                        child: Image(
-                                          image: new AssetImage(
-                                              "assets/Document.png"),
-                                          height: size.height / 14,
-                                          fit: BoxFit.contain,
-                                        ),
-                                      ),
-                                      Padding(
-                                        padding: EdgeInsets.only(top: 18.0),
-                                      ),
-                                      Text(
-                                        "Documents",
-                                        style: TextStyle(
-                                            fontSize: 13, color: Colors.grey),
-                                      ),
-                                    ],
-                                  ),
-                                  onTap: () {},
-                                ),
-                              ],
-                            ),
-                            Center(
-                              child: Divider(
-                                color: Colors.grey,
-                                //height: 30.0,
-                              ),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[
-                                InkWell(
-                                  splashColor: Colors.red,
-                                  child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: <Widget>[
-                                      Padding(
-                                        padding: const EdgeInsets.fromLTRB(
-                                            110, 5, 10, 8),
-                                        child: Image(
-                                          image: new AssetImage(
-                                              "assets/icon5.png"),
-                                          height: size.height / 12.5,
-                                        ),
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.fromLTRB(
-                                            100, 15, 10, 15),
-                                        child: Text(
-                                          "Set Meetings",
-                                          style: TextStyle(
-                                              fontSize: 13, color: Colors.grey),
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                  onTap: () {},
-                                ),
-                                InkWell(
-                                  splashColor: Colors.red,
-                                  child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: <Widget>[
-                                      Padding(
-                                        padding: const EdgeInsets.fromLTRB(
-                                            50, 6, 0, 10),
-                                        child: Image(
-                                          image: new AssetImage(
-                                              "assets/icon3.png"),
-                                          height: size.height / 14,
-                                          fit: BoxFit.contain,
-                                          //width: 50.0,
-                                        ),
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.fromLTRB(
-                                            40, 20, 0, 13),
-                                        child: Text(
-                                          "Upload Documents",
-                                          style: TextStyle(
-                                              fontSize: 13, color: Colors.grey),
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                  onTap: () {},
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: <Widget>[],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ]),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 2),
-                ),
-                GestureDetector(
-                    onTap: () {}, // code hack do nothing
-                    child: Wrap(children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.fromLTRB(0.0, 1, 0.0, 2),
-                        height: size.height / 4.8,
-                        child: ClipRRect(
-                            borderRadius: BorderRadius.circular(0.0),
-                            child: carousel2),
-                      ),
-                    ]))
+                
+               
               ],
             ),
           ),
-        ),
-      
+          Container(
+            //color: Colors.black,
+           
+            child: Wrap(
+              children: <Widget>[
+              Container(
+              height: size.height/2.5,
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10.0), color: Colors.white),
+                margin: EdgeInsets.fromLTRB(5, 5, 5, 5),
+               // color: Colors.white,
+                padding: EdgeInsets.fromLTRB(40,40,40,40),
+                //color: Colors.black,
+               // width: size.width,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            Image(
+                              image: new AssetImage("assets/icon1.png"),
+                              height: size.height/13,
+                              
+                              
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0,5,0,5),
+                              child: Text("Tasks"),
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Image(
+                              image: new AssetImage("assets/invoice.png"),
+                              height: size.height/13,
+                              
+                              
+                              
+
+                            ),
+                            
+                            Padding(
+                           padding: const EdgeInsets.fromLTRB(0,5,0,5),
+                              child: Text("Invoice"),
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Image(
+                              image: new AssetImage("assets/Document.png"),
+                              height: size.height/13,
+                              
+                            ),
+                            Padding(
+                       padding: const EdgeInsets.fromLTRB(0,5,0,5),
+                              child: Text("Documents"),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                    
+                    //  padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                      Divider(
+                        color: Colors.grey,
+                        height: 50.0,
+                       
+
+                      ),
+                    
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+
+                        Column(
+                          children: <Widget>[
+                            Image(
+                              image: new AssetImage("assets/icon5.png"),
+                              height: size.height/13,
+                              
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0,5,0,5),
+                              child: Text("Set Meetings"),
+                            )
+                          ],
+                        ),
+
+                        Column(
+                          children: <Widget>[
+                            Image(
+                              image: new AssetImage("assets/icon3.png"),
+                              height: size.height/13,
+                              
+                            ),
+                            Padding(
+                             padding: const EdgeInsets.fromLTRB(0,5,0,5),
+                              child: Text("Upload documents"),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              
+              ),
+              ]
+            ),
+          ),
+          GestureDetector(
+                  onTap: (){},  // code hack do nothing
+                  child: Wrap(
+                    children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                     height: size.height/4.8,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0), 
+                          child: carousel2
+                    ),
+                ),
+        ],
+
+      ),
+    ),
+        ]
+      )
+        ) 
+    ),
     );
+        
   }
 }
--- lib/src_code/page.dart
+++ lib/src_code/page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:snbiz/src_code/profile.dart';
+import 'package:snbiz/src_code/static.dart';
 import 'nav.dart';
 
 import 'meeting.dart' as second;
@@ -39,7 +41,7 @@ class MainPageState extends State<MainPage> with SingleTickerProviderStateMixin 
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: new Center(child: new Text("Kathmandu Codes"), 
+        title: new Center(child: Text(StaticValue.orgName), 
       
         ),
         backgroundColor: const Color(0xFF9C38FF),
@@ -47,15 +49,26 @@ class MainPageState extends State<MainPage> with SingleTickerProviderStateMixin 
          actions: <Widget>[
             // action button
             IconButton(
-              padding: const EdgeInsets.only(right: 30.0),
+              padding: const EdgeInsets.only(right: 8.0),
               icon: Icon(Icons.notifications, size: 30.0),
               onPressed: () {},
             ),
 
             Padding(
-              padding: const EdgeInsets.only(right: 18),
-              child: CircleAvatar(
+              padding: const EdgeInsets.fromLTRB(0,10,15,10),
+              child: GestureDetector(
+                onTap: (){
+                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>Profile()));
+                },
+                child: CircleAvatar(
+               radius: 20,
+               
                 
+              
+               backgroundImage: NetworkImage(StaticValue.logo, ),
+                
+                  
+                ),
               ),
             )
                         ]
@@ -78,26 +91,31 @@ class MainPageState extends State<MainPage> with SingleTickerProviderStateMixin 
                                   bottomNavigationBar: new Material(
                                     color: Color(0xFFBF9b38ff),
 
-                                    child: Container(
-                                      height: size.height/14,
-                                      
-                                      child: TabBar(
+                                    
+                                                         child:      Container(
+                                        height: size.height/12,
                                         
-                                        controller: controller,
-                                        tabs: <Tab>[
+                                        child: TabBar(
+                                          
+                                          controller: controller,
+                                          tabs: <Tab>[
 
-                                          new Tab(icon: new Icon(Icons.home, size: 18,), text:"Home",),
-                                          new Tab(icon: new Icon(Icons.people,size: 18), text: "Meeting",),
-                                          new Tab(icon: new Icon(Icons.camera_alt, size: 18), text: "Upload",),
-                                          new Tab(icon: new Icon(Icons.person, size: 18,), text:"Profile",),
+                                            new Tab(icon: new Icon(Icons.home, size: 16,), text:"Home",),
+                                            new Tab(icon: new Icon(Icons.people,size: 16), text: "Meeting",),
+                                            new Tab(icon: new Icon(Icons.camera_alt, size: 16), text: "Upload",),
+                                            new Tab(icon: new Icon(Icons.person, size: 16,), text:"Profile",),
               
-                                        ],
+                                          ],
+                                        ),
                                       ),
+                                    
                                     ),
-                                  ),
+    );
+                                  
+                                  
                             
                             
-                                );
+                                
                                 
                               }
               
